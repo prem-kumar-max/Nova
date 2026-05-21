29937663db54e3e2e20d2fa187c124edcf9cd2d9
prem-kumar-max <prem64748494@gmail.com>
2026-05-21 21:05:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <center>
+        <h1>Welcome to React....</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis soluta error dolorum assumenda, magnam amet vero ex quisquam, enim exercitationem impedit repellendus doloremque distinctio esse cumque ipsa velit eaque neque!  </p>
+      </center>
+    </>
+  )
+}
+
+export default App
