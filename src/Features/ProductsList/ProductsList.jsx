6bbccf719e774038821010cd315dd8b20120b784
prem-kumar-max <prem64748494@gmail.com>
2026-07-10 20:41:44 +0530

    Add hover effects to product cards

--- src/Features/ProductsList/ProductsList.jsx
+++ src/Features/ProductsList/ProductsList.jsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react"
 export const ProductList = () => {
     let [products, setProducts] = useState([])
     let [categoryList, setCategoryList] = useState([])
-    let [category,setCategory]=useState("")
-    let [search,setSearch]=useState("")
+    let [category, setCategory] = useState("")
+    let [search, setSearch] = useState("")
 
     useEffect(() => {
         async function categoryData() {
@@ -17,16 +17,16 @@ export const ProductList = () => {
         categoryData()
     }, [])
 
-    useEffect(() => {
-        async function apiData() {
-            let a = fetch("https://dummyjson.com/products")
-            let b = await a;
-            let { products } = await b.json()
-            console.log(products)
-            setProducts(products)
-        }
-        apiData()
-    }, [])
+    // useEffect(() => {
+    //     async function apiData() {
+    //         let a = fetch("https://dummyjson.com/products")
+    //         let b = await a;
+    //         let { products } = await b.json()
+    //         console.log(products)
+    //         setProducts(products)
+    //     }
+    //     apiData()
+    // }, [])
     //     let [products,setProducts]=useState
     // useEffect(()=>{
     //     async function apiData(){
@@ -38,22 +38,54 @@ export const ProductList = () => {
     //     }
     //     apiData()
     // },[])
+    useEffect(() => {
+        async function threeApi() {
+            let api;
+            if (category) {
+                api = `https://dummyjson.com/products/category/${category}`
+            }
+            else if (search) {
+                api = `https://dummyjson.com/products/search?q=${search}`
+            }
+            else {
+                api = `https://dummyjson.com/products`
+            }
+            let { data } = await axios.get(api)
+            setProducts(data.products)
+        }
+        threeApi()
+
+    }, [category, search])
     return (
         <>
             <center><h1 className="mb-3 mt-3">Products Data</h1></center>
             <div className="container mt-3">
                 <div className="row justify-content-between">
                     <div className="col-5">
-                        <input type="text" className="form-control" placeholder="Search product.." />
+                        <input type="text" className="form-control" placeholder="Search product.."
+                            onChange={
+                                (e) => {
+                                    setSearch(e.target.value)
+                                    setCategory("")
+                                }
+                            }
+                        />
                     </div>
                     <div className="col-5">
-                        <select name="" id="" className="form-control">
-                        <option value="ALL CATEGORIES">ALL CATEGORIES</option>
-                        {
-                            categoryList.map((item,i)=>(
-                                <option key={i} value={item}>{item}</option>
-                            ))
-                        }</select>
+                        <select name="" id="" className="form-control"
+                            onChange={
+                                (e) => {
+                                    setCategory(e.target.value)
+                                    setSearch("")
+                                }
+                            }
+                        >
+                            <option value="ALL CATEGORIES">ALL CATEGORIES</option>
+                            {
+                                categoryList.map((item, i) => (
+                                    <option key={i} value={item}>{item}</option>
+                                ))
+                            }</select>
 
                     </div>
 
@@ -63,8 +95,8 @@ export const ProductList = () => {
             <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
                 {
                     products.map((item) => (
-                        <div style={{ width: "250px", border: "2px solid", borderRadius: "10px", padding: "8px", margin: "20px", textAlign: "center", }}>
-                            <img src={item.thumbnail} alt="" height={150} />
+                        <div className="product-card" key={item.id}>
+                            <img src={item.thumbnail} alt={item.title} height={150} />
                             <h1>{item.title}</h1>
                             <p>{item.description}</p>
                             <mark>{item.price}</mark>
